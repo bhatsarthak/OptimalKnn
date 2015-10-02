4030215cc55ac249ce14256af0f0a21b092cf805
Sarthak Bhat <sarthakbhat@localhost.localdomain>
2015-10-01 22:49:33 -0400

    Adding main file,location class and input data

---
+++ src/LocationData.java
@@ -0,0 +1,22 @@
+
+public class LocationData {
+	private int placeId;
+	private double latitude;
+	private double longitude;
+	private String category;
+	private int uniqueUusers;
+	private int checkIns;
+	private String title;
+
+	//setters and getters for the given parameters
+	void setplaceId(int placeId) {
+		this.placeId = placeId;
+	}
+	
+	int getplaceId() {
+		return this.placeId;
+	}
+	
+	//TODO:Add setters and getters for the remaing parameters
+	
+}
---
+++ src/OptimalRetailStore.java
@@ -0,0 +1,10 @@
+
+public class OptimalRetailStore {
+
+	public static void main(String[] args) {
+		// TODO:Read the store location data and input it into hash map with store ID as key and LocationData object as value
+		
+
+	}
+
+}
